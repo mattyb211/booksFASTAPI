7ca7e2fe79d206361ec5a54c6acf3659cb630b5f
Mateo Balzola <mateobalzola@crc-dot1x-nat-10-239-162-79.bu.edu>
2025-02-27 18:04:55 -0500

    CREATED main and models files

---
+++ models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Book(BaseModel):
+    id: Optional[int] = None
+    title: str
+    author: str
+    description: Optional[str] = None
+
+
